f3345e31ea63a2fc4f3071fa49b3ef6e4ecd34e9
Dima Neor <dima.bulatov.deman@gmail.com>
2021-11-29 00:07:13 +0400

    вычисление задания по кнопке

--- lab1_1/lab1.py
+++ lab1_1/lab1.py
@@ -1,8 +1,9 @@
 import math
 import tkinter as tk
-
-n = 0
-p = 0
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
 def h(x):
     if x == 0 : return x
@@ -13,13 +14,77 @@ def entropy(pArray):
     sum = 0
     for i in pArray :
         sum += h(i)
-    return sum
+    return -sum
 
 # Формула Бернули
 def bern (p, n, k):
     # math.comb = n! / (k! * (n - k)!)
     # p**k = возведение в степень 
     return math.comb(n, k) * (p**k) * ((1 - p)**(n - k))
+
+# Обработчик кнопки, вычисление энтропии
+def calculate ():
+    p = float(inP_ent.get())
+    n = int(inN_ent.get())
+    if (p > 1 or p < 0):
+        print("Ошибка")
+    else:
+        pList = []
+        kList = []
+        for k in range(n+1):
+            pi = bern(p, n, k)
+            pi = float(f"{pi:.3f}")
+            kList.append(k)
+            pList.append(pi)
+ 
+        kStr = "k \t"
+        pStr = "p \t"
+        for i in range(len(kList)):
+            kStr += f"{kList[i]} \t"
+            pStr += f"{pList[i]} \t"
+        outK_lbl.configure(text=kStr)
+        outP_lbl.configure(text=pStr)
+        # Находим и выводим энтропию
+        entr = 0
+        if (p != 0 or p != 1):
+            entr = entropy(pList)
+        else:
+            entr = 0
+        outEntropy_lbl.configure(text=f"Энтропия: {entr:.3f}")
+
+        # Находим энтропию на всем промежутке p
+        probList = [0]
+        entropyList = [0]
+        curr_prob = 0.01
+        while (curr_prob < 1.01):
+            piList = []
+            for k in range(n+1):
+                pi = bern(curr_prob, n, k)
+                pi = float(f"{pi:.3f}")
+                piList.append(pi)
+            entr2 =  entropy(piList)
+            probList.append(curr_prob)
+            entropyList.append(entr2)
+            curr_prob += 0.01
+
+        clonEntropyList = entropyList.copy()
+        del clonEntropyList[0]
+        del clonEntropyList[-1]
+
+        outMin_lbl.configure(text=f"Минимум: {min(clonEntropyList):.3f}")
+        outMax_lbl.configure(text=f"Максимум: {max(clonEntropyList):.3f}")
+
+        fig = Figure(figsize=(4,4), dpi = 100)
+    
+        plt = fig.add_subplot(111)
+        plt.grid()      # включение отображение сетки
+        plt.plot(probList, entropyList)
+
+        canvas = FigureCanvasTkAgg(fig, master=frame3)
+        canvas.draw()
+
+        canvas.get_tk_widget().pack()
+
 
 window = tk.Tk()
 
@@ -41,7 +106,7 @@ inP_ent = tk.Entry(master=frame1)
 inP_ent.pack(side=tk.LEFT)
 
 # Создаем кнопку для вычисления значений
-enter_btn = tk.Button( master=window, text="Вычислить")
+enter_btn = tk.Button( master=window, text="Вычислить", command=calculate)
 enter_btn.pack(side=tk.TOP)
 
 # Создаем форму для вывода результата
